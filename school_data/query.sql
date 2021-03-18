3b3709a1915be9565330f9cca3b0b09f9499b574
das <don.a.sarge@gmail.com>
2021-03-18 07:16:40 -0400

    school data

---
+++ school_data/query.sql
@@ -0,0 +1,12 @@
+-- Create new table
+CREATE TABLE Graduation_data (
+	NCES_LEA_ID text(50),
+	State_ID text(50),
+	District_Name text(50),
+	Mailing_Address text(50),
+	City text(50),
+	State_ text(50),
+	Zip_Code text(50),
+	Zip_4 text(50) 
+);
+
