c9a613117594ba2316c4743caf0d1c1a125194b8
das <don.a.sarge@gmail.com>
2021-03-20 09:09:49 -0400

    cleaned school data

--- school_data/query.sql
+++ school_data/query.sql
@@ -1,12 +1,21 @@
--- Create new table
-CREATE TABLE Graduation_data (
-	NCES_LEA_ID text(50),
-	State_ID text(50),
-	District_Name text(50),
-	Mailing_Address text(50),
-	City text(50),
-	State_ text(50),
-	Zip_Code text(50),
-	Zip_4 text(50) 
-);
+-- Table: public.graduation_data
 
+ DROP TABLE public.graduation_data;
+
+CREATE TABLE public.graduation_data
+(
+    nces_lea_id character varying(50) COLLATE pg_catalog."default",
+    state_id character varying(50) COLLATE pg_catalog."default",
+    district_name character varying(50) COLLATE pg_catalog."default",
+    mailing_address character varying(50) COLLATE pg_catalog."default",
+    city character varying(50) COLLATE pg_catalog."default",
+    state_abbr character varying(50) COLLATE pg_catalog."default",
+    zip_code character varying(50) COLLATE pg_catalog."default",
+    zip_4 character varying(50) COLLATE pg_catalog."default",
+	telephone character varying(50) COLLATE pg_catalog."default"
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.graduation_data
+    OWNER to postgres;
